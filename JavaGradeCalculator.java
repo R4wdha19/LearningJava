76289e20a95def4182263190c21454e9af2730a1
Rawdha Alyhayia <rweedh@hotmail.com>
2024-10-25 16:39:08 +0400

    Reformatted the code, Refactored and updated the conditions

--- JavaGradeCalculator.java
+++ JavaGradeCalculator.java
@@ -2,27 +2,23 @@ import java.util.Scanner;
 
 public class JavaGradeCalculator {
 
-    public static void main (String[] args){
-Calculator();
-
+    public static void main(String[] args) {
+        calculator();
     }
-    public static void Calculator(){
+
+    public static void calculator() {
         Scanner input = new Scanner(System.in);
         System.out.println("Please Enter The Score To Get The Grade ");
         int score = input.nextInt();
-        if(score>= 90) {
+        if (score >= 90 && score<100) {
             System.out.println("Excellent You Got A");
-        }
-        else if (score>= 80 && score<90) {
+        } else if (score >= 80 && score < 90) {
             System.out.println("Great Work You Got B");
-        }
-        else if (score>= 70 && score<80) {
+        } else if (score >= 70 && score < 80) {
             System.out.println("Good Work You Got C");
-        }
-        else if (score>= 60 && score<70) {
+        } else if (score >= 60 && score < 70) {
             System.out.println("Need To Study More ! You Got D");
-        }
-        else {
+        } else {
             System.out.println("Sorry you have failed");
         }
     }
