f8ede7adb69e8ca34c9106bc64252556d79c4aa8
Rawdha Alyhayia <rweedh@hotmail.com>
2024-10-25 16:31:00 +0400

    Added code to check the score

--- JavaGradeCalculator.java
+++ JavaGradeCalculator.java
@@ -10,7 +10,21 @@ public class JavaGradeCalculator {
         Scanner input = new Scanner(System.in);
         System.out.println("Please Enter The Score To Get The Grade ");
         int score = input.nextInt();
-
+        if(score> 90) {
+            System.out.println("Excellent You Got A");
+        }
+        else if (score> 80 && score<90) {
+            System.out.println("Great Work You Got B");
+        }
+        else if (score> 70 && score<80) {
+            System.out.println("Good Work You Got C");
+        }
+        else if (score> 60 && score<70) {
+            System.out.println("Need To Study More ! You Got D");
+        }
+        else {
+            System.out.println("Sorry you have failed");
+        }
     }
 
 }
