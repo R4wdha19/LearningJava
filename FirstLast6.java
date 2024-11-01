7fa895367e55d4d5eb2c69c2879a97c25f02cfd8
Rawdha Alyhayia <rweedh@hotmail.com>
2024-11-01 15:20:08 +0400

    formatted the code

--- FirstLast6.java
+++ FirstLast6.java
@@ -3,10 +3,10 @@ import java.util.List;
 
 public class FirstLast6 {
 
-    public static void firstLast6(){
-        int [] firstNumbersSet = {1,2,3,4,5,6};
-        int [] secondNumbersSet = {6,5,4,3,2,1};
-        int [] thirdNumbersSet = {1,2,3,6,5,4};
+    public static void firstLast6() {
+        int[] firstNumbersSet = {1, 2, 3, 4, 5, 6};
+        int[] secondNumbersSet = {6, 5, 4, 3, 2, 1};
+        int[] thirdNumbersSet = {1, 2, 3, 6, 5, 4};
 
     }
 }
