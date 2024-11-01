9e897b6a356b35342c8490f16655141c09714b3b
Rawdha Alyhayia <rweedh@hotmail.com>
2024-11-01 15:23:36 +0400

    Defined the number to be checked

--- FirstLast6.java
+++ FirstLast6.java
@@ -7,6 +7,8 @@ public class FirstLast6 {
         int[] firstNumbersSet = {1, 2, 3, 4, 5, 6};
         int[] secondNumbersSet = {6, 5, 4, 3, 2, 1};
         int[] thirdNumbersSet = {1, 2, 3, 6, 5, 4};
+        int target = 6;
+
 
     }
 }
