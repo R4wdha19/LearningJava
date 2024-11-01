2f42324422f794cebf5e37cb2527084788427a5b
Rawdha Alyhayia <rweedh@hotmail.com>
2024-11-01 15:38:39 +0400

    simplified the code

--- FirstLast6.java
+++ FirstLast6.java
@@ -2,26 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FirstLast6 {
-    public static void main(String[]  args){
+    public static void main(String[] args) {
         firstLast6();
     }
 
     public static void firstLast6() {
-        int[] firstNumbersSet = {1, 2, 3, 4, 5, 6};
-        int[] secondNumbersSet = {6, 5, 4, 3, 2, 1};
-        int[] thirdNumbersSet = {6, 2, 3, 6, 5, 6};
-        int[] fourthNumbersSet = {1, 2, 3, 6, 5, 7};
+        int[] numbers = {1, 2, 3, 4, 5, 6};
         int target = 6;
 
-        if (firstNumbersSet[0] == target || firstNumbersSet[firstNumbersSet.length - 1] == target) {
+        if (numbers[0] == target || numbers[numbers.length - 1] == target) {
             System.out.println("TRUE");
-        } else if (secondNumbersSet[0] == target || secondNumbersSet[secondNumbersSet.length - 1] == target) {
-            System.out.println("TRUE");
-        } else if (thirdNumbersSet[0] == target || thirdNumbersSet[thirdNumbersSet.length - 1] == target) {
-            System.out.println("TRUE");
-        } else if (fourthNumbersSet[0] == target || fourthNumbersSet[fourthNumbersSet.length - 1] == target) {
-            System.out.println("TRUE");
-        } else {
+        }
+        else {
             System.out.println("FALSE");
         }
 
