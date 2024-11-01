f158eda9d749837acc5993c409ae9b0f96d3d6c7
Rawdha Alyhayia <rweedh@hotmail.com>
2024-11-01 17:18:38 +0400

    Added a functionality

--- Arrays.java
+++ Arrays.java
@@ -1,7 +1,10 @@
 public class Arrays {
     public static void main(String[] args) {
-       // firstLast6();
+        // firstLast6();
+/*
         commonEnd();
+*/
+        swapEnds();
     }
 
     public static void firstLast6() {
@@ -22,14 +25,22 @@ public class Arrays {
 
         if (firstNumbers[0] == secondNumbers[0] || firstNumbers[firstNumbers.length - 1] == secondNumbers[secondNumbers.length - 1]) {
             System.out.println("TRUE");
-        }
-        else {
+        } else {
             System.out.println("FALSE");
         }
     }
 
-    public static void swapEnds(){
+    public static void swapEnds() {
         int[] numbers = {1, 2, 3, 4, 5, 6};
+        int firstNumber = numbers[0];
+        int secondNumber = numbers[numbers.length - 1];
+        numbers[numbers.length - 1] = firstNumber;
+        numbers[0] = secondNumber;
+        for(int x: numbers){
+            System.out.println(x);
+        }
+
+
     }
 
 }
